0a4d16dc2521978237158ddaaca352cb72d0b0fb
Richard Koranteng <e459189@us.lmco.com>
2025-03-05 14:04:01 -0500

    change: update the footer menu

--- modules/footer.js
+++ modules/footer.js
@@ -2,10 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const footer = `
       <footer>
-        <p>&copy; 2023 Free DBA Learning. All rights reserved.</p>
+        <p>&copy; 2025 Learn with RK. All rights reserved.</p>
         <ul class="footer-links">
-          <li><a href="#">Privacy Policy</a></li>
-          <li><a href="#">Terms of Service</a></li>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Courses</a></li>
           <li><a href="contact.html">Contact Us</a></li>
         </ul>
       </footer>
