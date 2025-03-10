5c9cf3f122c07a144d454dcb95c3a260e85a99ac
Richard Koranteng <richard@rkkoranteng.com>
2025-03-10 12:57:54 -0400

    change copyright link font color

--- modules/footer.js
+++ modules/footer.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const footer = `
       <footer>
-        <p>&copy; 2025 <a href=https://rkkoranteng.com>Rchard Koranteng</a>. All rights reserved.</p>
+        <p>&copy; 2025 <a href=https://rkkoranteng.com><font color="#007BFF">Richard Koranteng</a></font>. All rights reserved.</p>
         <ul class="footer-links">
           <li><a href="file:///C:/Users/richa/OneDrive/Desktop/github/modern-dba-project/terms.html">Terms</a></li>
           <li><a href="file:///C:/Users/richa/OneDrive/Desktop/github/modern-dba-project/course.html">Courses</a></li>
