04b8926a91dba2221fd9c4a521b9eb53c71dfbe5
Richard Koranteng <richard@rkkoranteng.com>
2025-03-10 12:33:04 -0400

    change footer a href to color white

--- modules/footer.js
+++ modules/footer.js
@@ -2,11 +2,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const footer = `
       <footer>
-        <p>&copy; 2025 Learn with RK. All rights reserved.</p>
+        <p>&copy; 2025 <a href=https://rkkoranteng.com>Rchard Koranteng</a>. All rights reserved.</p>
         <ul class="footer-links">
           <li><a href="file:///C:/Users/richa/OneDrive/Desktop/github/modern-dba-project/terms.html">Terms</a></li>
-          <li><a href="course.html">Courses</a></li>
-          <li><a href="contact.html">Contact Us</a></li>
+          <li><a href="file:///C:/Users/richa/OneDrive/Desktop/github/modern-dba-project/course.html">Courses</a></li>
+          <li><a href="file:///C:/Users/richa/OneDrive/Desktop/github/modern-dba-project/about.html">About</a></li>
         </ul>
       </footer>
     `;
